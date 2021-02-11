6d20dbb61113841d7433f13db0c1f7147e86067f
aes123456 <74045412+aes123456@users.noreply.github.com>
2021-02-11 17:22:31 +0200

    Update Weather.js

--- geolocation/weather/src/Weather.js
+++ geolocation/weather/src/Weather.js
@@ -3,7 +3,7 @@ import React,{useState, useEffect} from 'react'
 
 const API_URL = 'https://api.openweathermap.org/data/2.5/onecall?';
 const ICON_URL = 'http://openweathermap.org/img/wn/';
-const API_KEY = '16663432b319157d87567c3b0e19e0e3';
+const API_KEY = '';
 
 
 export default function Weather({lat,lng}) {
